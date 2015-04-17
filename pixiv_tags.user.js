336216b2b9264e7b763130640cb81640eba629bd
oui <hedgehog.oui@gmail.com>
2015-04-18 01:27:36 +0900

    短縮表示、常に展開表示できるように

--- pixiv_tags.user.js
+++ pixiv_tags.user.js
@@ -9,7 +9,7 @@
 // @grant       GM_registerMenuCommand
 // @grant       GM_getValue
 // @grant       GM_setValue
-// @version     1.04
+// @version     1.05
 // ==/UserScript==
 
 /* jshint multistr: true */
@@ -18,90 +18,94 @@
 'use strict';
 
 var SCRIPT_NAME = 'pixiv Tags';
+var TAGLIST_ID = SCRIPT_NAME.replace(/ /g, '');
 
 function setStyle() {
   var style =
-    '#pixivTags' +
-    '{' +
+    '#'+TAGLIST_ID + '{' +
+    '  background-color : #FFF;' +
+    '  border: 1px solid #D6DEE5;' +
+    '  border-radius: 5px;' +
+    '  padding : 3px;' +
+    '  line-height : 1.7em;' +
+    '  overflow : hidden;' +
+    '  box-sizing: border-box;' +
+    '}' +
+    
+    // マウスオンで表示するタイプ
+    '.show-float {' +
     '  position : fixed;' +
     '  left : 10px;' +
     '  top : 10px;' +
     '  width : 8em;' +
-    '  height : 2em;' +
-    '  background-color : #FFF;' +
-    '  border: 1px solid #D6DEE5;' +
-    '  border-radius: 5px 5px 5px 5px;' +
-    '  padding : 5px;' +
-    '  line-height : 1.7em;' +
-    '  z-index : 998;' +
-    '  overflow : hidden;' +
+    '  height : 3em;' +
     '  opacity : 0.8;' +
+    '  z-index : 998;' +
     '}' +
 
-    '#pixivTags li' +
-    '{' +
-    '  display : none;' +
-    '}' +
-
-    '#pixivTags:hover' +
-    '{' +
-    '  width : 300px;' +
-    '  height : 100%;' +
-    '  opacity : 1;' +
-    '}' +
-
-    '#pixivTags:hover li' +
-    '{' +
-    '  display : inline-block;' +
+    '.show-float:hover {' +
+    '  min-width : 300px;' +
+    '  height : auto;' +
+    '  opacity : 0.9;' +
     '}' +
 
     '@media screen and (min-width : 1450px) {' +
-    '  #pixivTags, #pixivTags:hover' +
-    '  {' +
+    '  .show-float, .show-float:hover {' +
     '    width : 200px;' +
     '    height : 100%;' +
     '    opacity : 1;' +
     '  }' +
 
-    '  #pixivTags li' +
-    '  {' +
+    '  .show-float li {' +
     '    display : inline-block;' +
     '  }' +
     '}' +
 
     '@media screen and (min-width : 1550px) {' +
-    '  #pixivTags, #pixivTags:hover' +
-    '  {' +
+    '  .show-float, .show-float:hover {' +
     '    width : 250px;' +
     '  }' +
     '}' +
 
     '@media screen and (min-width : 1650px) {' +
-    '  #pixivTags, #pixivTags:hover' +
-    '  {' +
+    '  .show-float, .show-float:hover {' +
     '    width : 300px;' +
     '  }' +
     '}' +
 
     '@media screen and (min-width : 1750px) {' +
-    '  #pixivTags, #pixivTags:hover' +
-    '  {' +
+    '  .show-float, .show-float:hover {' +
     '    width : 350px;' +
     '  }' +
     '}' +
 
     '@media screen and (min-width : 1850px) {' +
-    '  #pixivTags, #pixivTags:hover' +
-    '  {' +
+    '  .show-float, .show-float:hover {' +
     '    width : 400px;' +
     '  }' +
     '}' +
 
     '@media screen and (min-width : 1920px) {' +
-    '  #pixivTags, #pixivTags:hover' +
-    '  {' +
+    '  .show-float, .show-float:hover {' +
     '    width : 435px;' +
     '  }' +
+    '}' +
+
+    // 常に展開して表示するタイプ
+    '.show-always {' +
+    '  position : relative;' +
+    '  margin-bottom : 5px;' +
+    '  width : 100%;' +
+    '  height : auto;' +
+    '}' +
+    
+    // ボタンと親要素の調整
+    '#'+TAGLIST_ID+' button {' +
+    '  float : right;' +
+    '  margin-top : 0.3em;' +
+    '}' + 
+    '#'+TAGLIST_ID+' h1 {' +
+    '  line-height: 2.7em;' +
     '}';
 
   GM_addStyle(style);
@@ -125,7 +129,7 @@ function optimizeTags() {
 }
 
 function generateHTML() {
-  var html = '<h1 class="unit-title">' + SCRIPT_NAME + '</h1><ul class="tags">';
+  var html = '';
 
   var tags = GM_config.get('tags');
   tags = tags.split('\n');
@@ -140,35 +144,66 @@ function generateHTML() {
 
     // タグ前後のスペースを削除
     tags[i] = tags[i].replace(/(^ +| +$)/g, '');
+    
+    // 短縮表示
+    var pattern = /(-{2,})+(\d{1,})$/;
+    var name = tags[i];
+    if(name.match(pattern)){
+      name = RegExp.$2 < (name.length - RegExp.lastMatch.length + 1) ? name.slice(0,   RegExp.$2) + '...' : name.slice(0, - RegExp.lastMatch.length - 1);
+    }
 
-    // URLににエンコードしたタグを追加
-    url += encodeURI(tags[i].replace(/ /g, '+'));
+    // URLに短縮数字除去+エンコードしたタグを追加
+    url += encodeURI(tags[i].replace(/ /g, '+').replace(pattern, '').replace(/[+-]$/, ''));
 
-    html += '<li class="tag"><a class="portal" href="' + url + '">c</a><a class="text" href="' + url + '">' + tags[i] + '</a></li>\n';
+    html += '<li class="tag"><a class="text" href="' + url + '" title="' + tags[i] + '"><span class="portal">c</span>' + name + '</a></li>\n';
   }
-
-  html += '</ul>';
 
   return html;
 }
 
 function updateHTML() {
-  var html = generateHTML();
-  var id = SCRIPT_NAME.replace(/ /g, '');
-
   var parentNode = document.getElementById('wrapper');
-  if (parentNode == null) {
+  
+  // 挿入先が見当たらなければ何もしない
+  if(parentNode == null){
     return;
   }
-
-  var div = document.getElementById(id);
-  if (div == null) { // null or undefined
-    div = document.createElement('div');
-    div.id = id;
-    parentNode.appendChild(div);
+  
+  // タグリストが生成済みなら中身だけ書き換え、なければ作成
+  var taglist = document.getElementById('tags');
+  if(taglist != null){
+    taglist.innerHTML = generateHTML();
+  }else{
+    var parent = document.createElement('div');
+    parent.id = TAGLIST_ID;
+    parent.innerHTML = '<h1 class="unit-title">' + SCRIPT_NAME + '</h1>';
+    var btn1 = document.createElement('button');
+    btn1.id = 'button-settings';
+    btn1.className = '_button';
+    btn1.textContent = '設定';
+    btn1.addEventListener('click', function(){GM_config.open();}, false);
+    parent.firstChild.appendChild(btn1);
+    var btn2 = document.createElement('button');
+    btn2.id = 'button-addtag';
+    btn2.className = '_button';
+    btn2.textContent = '検索条件を追加';
+    btn2.addEventListener('click', function(){addTag();}, false);
+    parent.firstChild.appendChild(btn2);
+    var ul = document.createElement('ul');
+    ul.id = 'tags';
+    ul.className = 'tags';
+    ul.innerHTML = generateHTML();
+    parent.appendChild(ul);
+    parentNode.insertBefore(parent, parentNode.firstChild);
   }
-
-  div.innerHTML = html;
+  
+  // 表示設定を切り替え
+  var taglist_container = document.getElementById(TAGLIST_ID);
+  if(GM_config.get('showAlways')){
+    taglist_container.className = 'show-always';
+  }else{
+    taglist_container.className = 'show-float';
+  }
 }
 
 function addTag() {
@@ -199,6 +234,11 @@ GM_config.init(
       cols : 60,
       rows : 20,
       default : 'Greasemonkeyの「ユーザスクリプトコマンド」でタグを設定できます。\nタグは1行に1つ書いて下さい。\n部分一致で検索したい場合は、タグの後ろにスペースを入れて下さい。\nAND/OR検索もできます。\n\n↓例↓\nオリジナル\nなにこれかわいい\n俺の 黒猫\nパチュリー OR パチェ'
+    },showAlways :
+    {
+      label : 'タグリストを常に展開して表示する',
+      type : 'checkbox',
+      default : false
     }
   },
   '#GM_config_field_tags{ width : 100%; }',
@@ -210,10 +250,9 @@ GM_config.init(
     }
   }
 );
-
 setStyle();
 updateHTML();
 GM_registerMenuCommand(SCRIPT_NAME + ' - 設定', function(){ GM_config.open(); });
-GM_registerMenuCommand(SCRIPT_NAME + ' - 現在表示中のタグを追加', function(){  addTag(); });
+GM_registerMenuCommand(SCRIPT_NAME + ' - 現在表示中のタグを追加', function(){ addTag(); });
 
 })();
